fda66d9676b17d068ba01f32a9218f01aecaaef6
MonsteRico <boilermonster0530@yahoo.com>
2022-11-26 14:46:12 -0500

    hide team names on mobile

--- src/pages/TeamLogo.tsx
+++ src/pages/TeamLogo.tsx
@@ -44,9 +44,7 @@ export default function TeamLogo({ team }: TeamLogoProps) {
         src={getTeamLogo(team)}
         className="transition duration-300 ease-in-out group-hover:scale-110"
       />
-      <h1 className="p-4 text-center sm:hidden md:block xl:block">
-        {teamName}
-      </h1>
+      <h1 className="hidden p-4 text-center md:block xl:block">{teamName}</h1>
     </div>
   );
 }
